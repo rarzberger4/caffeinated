12cdb66a1121ff56527a53da27b8a575b5725b1f
rarzberger4 <arzberger98@gmail.com>
2023-05-21 11:07:14 +0200

    added RecipiesViewModel and RecipiesViewModelFactory

--- app/src/main/java/com/example/caffeinated/viewmodels/RecipiesViewModel.kt
+++ app/src/main/java/com/example/caffeinated/viewmodels/RecipiesViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.caffeinated.viewmodels
 
-class RecipiesViewModel() {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.caffeinated.models.Recipe
+import com.example.caffeinated.repositories.RecipeRepo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+
+
+
+class RecipiesViewModel(private val repository: RecipeRepo): ViewModel()  {
+    private val _recipeListState = MutableStateFlow(listOf<Recipe>())
+    val recipeListState: StateFlow<List<Recipe>> = _recipeListState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            repository.getAllRecipes().distinctUntilChanged()
+                .collect{listOfRecipes ->
+                    if (listOfRecipes.isNullOrEmpty()){
+                        Log.d("RecipesViewModel", "Empty Recipes")
+                    }else{
+                        _recipeListState.value = listOfRecipes
+                    }
+
+                }
+        }
+    }
 }
