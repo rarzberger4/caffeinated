20034d10fe1b370e9e6ad6ce1051fd9c80e25bba
rarzberger4 <arzberger98@gmail.com>
2023-05-22 17:01:33 +0200

    Format Recipes and output a list

--- app/src/main/java/com/example/caffeinated/MainActivity.kt
+++ app/src/main/java/com/example/caffeinated/MainActivity.kt
@@ -14,9 +14,18 @@ import com.example.caffeinated.data.RecipeDatabase
 import com.example.caffeinated.repositories.RecipeRepo
 import com.example.caffeinated.ui.theme.CaffeinatedTheme
 import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.caffeinated.models.Recipe
 import com.example.caffeinated.viewmodels.RecipiesViewModel
 import com.example.caffeinated.viewmodels.RecipiesViewModelFactory
 
@@ -49,8 +58,7 @@ fun HomeScreen() {
 
 
 
-
-            Greeting(taskstate.value.toString())
+            RecipeList(recipeList = taskstate.value)
         }
     }
 }
@@ -69,5 +77,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     CaffeinatedTheme {
         Greeting("Andr")
+    }
+}
+
+@Composable
+fun RecipeList(recipeList: List<Recipe>) {
+    LazyColumn {
+        items(items = recipeList) { recipe ->
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = recipe.title,
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 18.sp),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+                Text(
+                    text = "Year: ${recipe.year}",
+                    style = TextStyle(fontSize = 16.sp),
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = "Rating: ${recipe.rating}",
+                    style = TextStyle(fontSize = 16.sp),
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = "Description: ${recipe.manual}",
+                    style = TextStyle(fontSize = 16.sp),
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/example/caffeinated/models/Recipe.kt
+++ app/src/main/java/com/example/caffeinated/models/Recipe.kt
@@ -32,7 +32,7 @@ fun getRecipes(): List<Recipe> {
             id = "1",
             title = "Rezept 1",
             year = "2023",
-            manual = "Put Coffee in water and enjoy",
+            manual = "Put Coffee in water and enjoy...",
             images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/cappucino-32dbfba.jpg"),
             rating = 9.9,
             comments = listOf("This is a Comment")
@@ -45,6 +45,78 @@ fun getRecipes(): List<Recipe> {
             images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/flat-white-3402c4f.jpg"),
             rating = 0.1,
             comments = listOf("This is another Comment")
+        ),
+        Recipe(
+            id = "3",
+            title = "Rezept 3",
+            year = "2022",
+            manual = "Mix milk, cocoa, and sugar. Heat and serve.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/hot-chocolate-1b8b262.jpg"),
+            rating = 8.5,
+            comments = listOf("Delicious hot chocolate recipe!")
+        ),
+        Recipe(
+            id = "4",
+            title = "Rezept 4",
+            year = "2010",
+            manual = "Blend fruits and yogurt to make a healthy smoothie.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/fruit-smoothie-350c52c.jpg"),
+            rating = 7.2,
+            comments = listOf("Refreshing smoothie recipe!")
+        ),
+        Recipe(
+            id = "5",
+            title = "Rezept 5",
+            year = "2021",
+            manual = "Grill the chicken and serve with veggies and sauce.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/grilled-chicken-1ef7f35.jpg"),
+            rating = 9.0,
+            comments = listOf("Perfectly grilled chicken!")
+        ),
+        Recipe(
+            id = "6",
+            title = "Rezept 6",
+            year = "2015",
+            manual = "Boil pasta, mix with sauce, and garnish with cheese.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/pasta-bolognese-9a058ad.jpg"),
+            rating = 8.7,
+            comments = listOf("Classic pasta recipe!")
+        ),
+        Recipe(
+            id = "7",
+            title = "Rezept 7",
+            year = "2020",
+            manual = "Bake the cake batter and frost with buttercream.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/victoria-sponge-0d6ce15.jpg"),
+            rating = 9.5,
+            comments = listOf("Moist and delicious cake recipe!")
+        ),
+        Recipe(
+            id = "8",
+            title = "Rezept 8",
+            year = "2019",
+            manual = "Marinate the steak and grill to perfection.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/steak-diane-5a180fe.jpg"),
+            rating = 9.2,
+            comments = listOf("Tender and juicy steak recipe!")
+        ),
+        Recipe(
+            id = "9",
+            title = "Rezept 9",
+            year = "2022",
+            manual = "Roast the vegetables and toss with dressing.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/roasted-vegetables-89ac8db.jpg"),
+            rating = 8.8,
+            comments = listOf("Healthy and flavorful vegetable recipe!")
+        ),
+        Recipe(
+            id = "10",
+            title = "Rezept 10",
+            year = "2018",
+            manual = "Make dough, add toppings, and bake the pizza.",
+            images = listOf("https://images.immediate.co.uk/production/volatile/sites/30/2020/08/pizza-8b55650.jpg"),
+            rating = 9.7,
+            comments = listOf("Homemade pizza recipe!")
         )
     )
 }
