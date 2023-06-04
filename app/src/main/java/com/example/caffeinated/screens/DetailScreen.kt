4f8e3ca4296cb491bcc83e7fdb03607bd0799093
rarzberger4 <raphael.arzberger@stud.fh-campuswien.ac.at>
2023-06-04 12:17:16 +0200

    backbutton color fixed partially

--- app/src/main/java/com/example/caffeinated/screens/DetailScreen.kt
+++ app/src/main/java/com/example/caffeinated/screens/DetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,11 +16,15 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -27,6 +32,7 @@ import androidx.navigation.NavController
 import com.example.caffeinated.data.RecipeDatabase
 import com.example.caffeinated.models.Recipe
 import com.example.caffeinated.repositories.RecipeRepo
+import com.example.caffeinated.ui.theme.CaffeinatedTheme
 import com.example.caffeinated.viewmodels.RecipeDetailViewModel
 import com.example.caffeinated.viewmodels.RecipeDetailViewModelFactory
 import com.example.caffeinated.widgets.RecipeRow
@@ -34,45 +40,48 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun DetailScreen(navController: NavController, recipeID: Long?) {
+    CaffeinatedTheme {
+        recipeID?.let {
+            val rr = RecipeRepo.getInstance(
+                RecipeDatabase.getDatabase(LocalContext.current).recipeDao()
+            )
 
-    recipeID?.let {
-        val rr = RecipeRepo.getInstance(
-            RecipeDatabase.getDatabase(LocalContext.current).recipeDao()
-        )
+            val factory = RecipeDetailViewModelFactory(rr, recipeID)
+            val viewModel: RecipeDetailViewModel = viewModel(factory = factory)
+            val recipe = viewModel.recipeState.collectAsState()
+            val coroutineScope = rememberCoroutineScope()
+            val scaffoldState = rememberScaffoldState() // this contains the `SnackbarHostState`
 
-        val factory = RecipeDetailViewModelFactory(rr, recipeID)
-        val viewModel: RecipeDetailViewModel = viewModel(factory = factory)
-        val recipe = viewModel.recipeState.collectAsState()
-        val coroutineScope = rememberCoroutineScope()
-        val scaffoldState = rememberScaffoldState() // this contains the `SnackbarHostState`
+            Surface(
+                color = androidx.compose.material3.MaterialTheme.colorScheme.background,
+                modifier = Modifier.fillMaxSize()
+            ) {
 
-        Scaffold(
-            scaffoldState = scaffoldState, // attaching `scaffoldState` to the `Scaffold`
-            topBar = {
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Button(
-                        onClick = { navController.popBackStack() },
-                        modifier = Modifier
-                            .padding(bottom = 8.dp)
-                            .fillMaxWidth()
-                    ) {
-                        androidx.compose.material3.Text("Back")
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Button(
+                            onClick = { navController.popBackStack() },
+                            modifier = Modifier
+                                .padding(bottom = 8.dp)
+                                .fillMaxWidth()
+                        ) {
+                            Text("Back")
+                        }
                     }
-                }
-            },
-        ) { padding ->
-            MainContent(
-                Modifier.padding(padding),
-                recipe.value
-            ) { recipe: Recipe ->
-                coroutineScope.launch {
-                    viewModel.updateFavoriteRecipe(recipe)
+
+                    MainContent(
+                        Modifier.padding(16.dp),
+                        recipe.value
+                    ) { recipe: Recipe ->
+                        coroutineScope.launch {
+                            viewModel.updateFavoriteRecipe(recipe)
+                        }
+                    }
                 }
             }
         }
     }
 }
-
 
 @Composable
 fun MainContent(
