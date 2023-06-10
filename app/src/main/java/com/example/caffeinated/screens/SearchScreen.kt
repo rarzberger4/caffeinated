a67d8025a678cff85a9ff8f8713dbae913c4b050
rarzberger4 <raphael.arzberger@stud.fh-campuswien.ac.at>
2023-06-10 15:47:08 +0200

    made some imporvements to the search

--- app/src/main/java/com/example/caffeinated/screens/SearchScreen.kt
+++ app/src/main/java/com/example/caffeinated/screens/SearchScreen.kt
@@ -1,11 +1,14 @@
 package com.example.caffeinated.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.TextField
 import androidx.compose.material3.MaterialTheme
@@ -17,10 +20,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -32,7 +37,7 @@ import com.example.caffeinated.viewmodels.RecipiesViewModelFactory
 import com.example.caffeinated.widgets.RecipeRow
 import kotlinx.coroutines.launch
 
-@Preview
+
 @Composable
 fun SearchScreen(navController: NavController = rememberNavController()) {
     val rr = RecipeRepo.getInstance(
@@ -49,7 +54,21 @@ fun SearchScreen(navController: NavController = rememberNavController()) {
             val roastinglvlinput = remember { mutableStateOf(TextFieldValue()) }
             val origininput = remember { mutableStateOf(TextFieldValue()) }
 
-            Column(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    androidx.compose.material3.Button(
+                        onClick = { navController.popBackStack() },
+                        modifier = Modifier
+                            .padding(bottom = 8.dp)
+                            .fillMaxWidth()
+                    ) {
+                        androidx.compose.material.Text("Back", color = Color.White)
+                    }
+                }
+
+
+
                 TextField(
                     value = roastinglvlinput.value,
                     onValueChange = { roastinglvlinput.value = it },
@@ -66,20 +85,17 @@ fun SearchScreen(navController: NavController = rememberNavController()) {
                         .fillMaxWidth()
                         .padding(16.dp)
                 )
+
+
+                if (roastinglvlinput.value.text != "" && origininput.value.text != "") {
+                    performSearch(
+                        viewModel,
+                        navController,
+                        roastinglvlinput.value.text.toInt(),
+                        origininput.value.text
+                    )
+                }
             }
-
-
-
-
-            if (roastinglvlinput.value.text != "" && origininput.value.text != "") {
-                performSearch(
-                    viewModel,
-                    navController,
-                    roastinglvlinput.value.text.toInt(),
-                    origininput.value.text
-                )
-            }
-
         }
     }
 }
